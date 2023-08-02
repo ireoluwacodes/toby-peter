dfc03f18b74aa17597262bf3c11475bf951c7af4
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2023-08-02 16:06:38 +0100

    get

--- routes/showRoute.js
+++ routes/showRoute.js
@@ -6,7 +6,7 @@ const showRouter = express.Router()
 
 showRouter.route("/create").post(authMiddleware, isAdmin, createShow)
 
-showRouter.route("/complete/:id").post(completeShow)
+showRouter.route("/complete/:id").get(completeShow)
 
 showRouter.route("/delete/:id").delete(authMiddleware, isAdmin, deleteShow)
 
