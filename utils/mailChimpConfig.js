ac4b86a026a05577eed0ea5e29d9628e76f04e3a
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2023-09-12 11:26:33 +0100

    chimp

--- utils/mailChimpConfig.js
+++ utils/mailChimpConfig.js
@@ -2,7 +2,7 @@ const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
-  server: "us21",
+  server: "us8",
 });
 
 const addToMailList = async (email) => {
